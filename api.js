7c12d92dcf18510981d401dcf981b0ae1f107572
VIRDAi <firdhanalamsyah7@gmail.com>
2025-09-16 11:15:03 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 <script>
-const WEB_APP_URL = 'https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLiPp-as0-gNv6HL6nNIqkJJSr7qrqK0wtdySf_YAi9zNKuFX40dtlNXZq5968VShLWf7CwgQBFFlA0JWUVUIvsULjac3mxVY89x1vkbhY_zEKADVgM2dqj2MOWI-9l9VjDG5833glt-L-QOQv_GK95kl89-ULOPF4tQaNNMTopXWcKk9BZWf5A7OrjYcoeEFXNkMr16Un_sXTSBUXfjx1GTV-Ga4GPP1rmX86YNo9t59QfFYUJ57jXjbb4uZYCC87wqkIDG1fD5BFzBgCYr1j6egTusQmYlNig5lpIwsWwZNAUWILc&lib=MR6uHCzNVGMmPDtdlHhpcGoLdMI28qpWa';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz9B0ACFB72a83Knm5DyJjRG8zRui08LsYRNXustRZsVmY73RVeUOAr3rSI8kkjRXDFbg/exec';
 
 async function apiGet(path, params={}){
   const q = new URLSearchParams({fn:path, ...params}).toString();
